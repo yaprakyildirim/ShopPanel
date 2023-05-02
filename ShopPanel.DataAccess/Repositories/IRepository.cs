d0f83c253b2cf64f7784c2204bada860e2abd6ae
yaprakyildirim <mrsyaprakyildirim@gmail.com>
2023-05-02 08:09:21 +0300

    Repository and UnitOfWork patterns applied.

---
+++ ShopPanel.DataAccess/Repositories/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using ShopPanel.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopPanel.DataAccess.Repositories
+{
+	public interface IRepository<T> where T : class, IEntityBase, new()
+	{
+		Task AddSync(T entity);
+		Task<List<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null, params Expression<Func<T, object>>[] includeProperties);
+
+		Task<T> GetAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties);
+		Task<T> GetByGuidAsync(Guid id);
+		Task<T> UpdateAsync(T entity);
+		Task DeleteAsync(T entity);
+		Task<bool> AnySync(Expression<Func<T, bool>> predicate);
+		Task<int> CountAsync(Expression<Func<T, bool>> predicate = null);
+	}
+}
---
+++ ShopPanel.DataAccess/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using ShopPanel.DataAccess.Context;
+using ShopPanel.DataAccess.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopPanel.DataAccess.UnitOfWorks
+{
+	public class UnitOfWork : IUnitOfWork
+	{
+		private readonly AppDbContext dbContext;
+		public UnitOfWork(AppDbContext dbContext)
+		{
+			this.dbContext = dbContext;
+		}
+		public async ValueTask DisposeAsync()
+		{
+			await dbContext.DisposeAsync();
+		}
+
+		public int Save()
+		{
+			return dbContext.SaveChanges();
+		}
+
+		public async Task<int> SaveAsync()
+		{
+			return await dbContext.SaveChangesAsync();
+		}
+
+		IRepository<T> IUnitOfWork.GetRepository<T>()
+		{
+			return new Repository<T>(dbContext);
+		}
+	}
+}
